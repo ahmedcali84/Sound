12ff6fb82c27c4eab86b025fdc51620f07cea6a2
Ahmed Ali <ahmedcali1984@gmail.com>
2024-11-25 20:00:04 +0300

    Add Helper Information

--- src/build.sh
+++ src/build.sh
@@ -4,3 +4,11 @@ CFLAGS=$(sdl2-config --cflags --libs)
 OUTPUT="sound"
 SOURCE="sound.cpp"
 g++ $SOURCE -o $OUTPUT $CFLAGS
+
+if [ $? -eq 0 ]; then
+    echo "[Success]: Compiled $SOURCE Successfully."
+    echo "RUN ./$OUTPUT -d to get Sound details."
+    echo "RUN ./$OUTPUT -v time(in milliseconds) to Get Sine Wave Visualization"
+else
+    echo "[ERROR]: Compilation Failed."
+fi
